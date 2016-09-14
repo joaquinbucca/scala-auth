71f0ff37c99e3a8b4f0bcfd49f26e0bc3fdf3330
Joaquin Bucca <joaquinbucca@Joaquins-MacBook-Pro.local>
2016-09-14 00:14:00 -0300

    just make curls work, no httping any external service although

--- src/main/scala/AkkaHttpMicroservice.scala
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -1,18 +1,11 @@
 import akka.actor.ActorSystem
 import akka.event.{LoggingAdapter, Logging}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.client.RequestBuilding
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
-import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.{ActorMaterializer, Materializer}
-import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
-import java.io.IOException
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import spray.json.DefaultJsonProtocol
 
@@ -23,18 +16,15 @@ case class AuthResponse(auth: Boolean, username: String, token: String)
 
 case class AuthRequest(username: String, token: String)
 
+case class RegisterRequest(username: String, password: String, repeated: String, name: String, lastname: String)
 
-object AuthChecker {
-//  def apply(username: String, password: String): AuthChecker = AuthChecker(calculateDistance(ip1Info, ip2Info), ip1Info, ip2Info)
 
-//  private def calculateDistance(ip1Info: IpInfo, ip2Info: IpInfo): Option[Double] = {  }
-
-}
 
 trait Protocols extends DefaultJsonProtocol {
   implicit val authRequestFormat = jsonFormat2(AuthRequest.apply)
   implicit val authResponseFormat = jsonFormat3(AuthResponse.apply)
   implicit val loginRequestFormat = jsonFormat2(LoginRequest.apply)
+  implicit val registerRequestFormat = jsonFormat5(RegisterRequest.apply)
 }
 
 trait Service extends Protocols {
@@ -45,39 +35,28 @@ trait Service extends Protocols {
   def config: Config
   val logger: LoggingAdapter
 
-  lazy val authApiConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
-    Http().outgoingConnection(config.getString("services.auth-api.host"), config.getInt("services.auth-api.port"))
-
-  def authApiRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(authApiConnectionFlow).runWith(Sink.head)
-
-  def fetchUserInfo(username: String): Future[Either[String, AuthResponse]] = {
-    authApiRequest(RequestBuilding.Get(s"/json/$username")).flatMap { response =>
-      response.status match {
-        case OK => Unmarshal(response.entity).to[AuthResponse].map(Right(_))
-        case BadRequest => Future.successful(Left(s"$username: incorrect IP format"))
-        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
-          val error = s"FreeGeoIP request failed with status code ${response.status} and entity $entity"
-          logger.error(error)
-          Future.failed(new IOException(error))
-        }
-      }
-    }
-  }
+  def register(registerRequest: RegisterRequest) : Future[AuthResponse] = Future(AuthResponse(true, registerRequest.username, "as"))
+  def login(loginRequest: LoginRequest) : Future[AuthResponse] = Future(AuthResponse(true, loginRequest.username, "logged"))
+  def authenticate(username: String, token: String) : Future[AuthResponse] = Future(AuthResponse(true, username, token))
 
   val routes = {
     logRequestResult("akka-http-microservice") {
       pathPrefix("auth") {
-        (get & path(Segment)) { auth =>
+        (get & path(Segment)) { username =>
           complete {
-            fetchUserInfo(auth).map[ToResponseMarshallable] {
-              case Right(authInfo) => authInfo
-              case Left(errorMessage) => BadRequest -> errorMessage
-            }
+            authenticate(username, "authhhh")
           }
         } ~
-        (post & entity(as[AuthRequest])) { authPairSummaryRequest =>
+        (post & entity(as[LoginRequest])) { loginRequest =>
           complete {
-            AuthResponse(true, authPairSummaryRequest.username, "super token")
+            login(loginRequest)
+          }
+        }
+      } ~
+      pathPrefix("register") {
+        (post & entity(as[RegisterRequest])) { registerRequest =>
+          complete {
+            register(registerRequest)
           }
         }
       }
